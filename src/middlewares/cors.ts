3b01e4f1d5d1482a4d9dc31303c661a458e0f7f5
Oybek <oybekmuslimov15@gmail.com>
2021-06-12 20:12:00 +0300

    create middlewares

---
+++ src/middlewares/cors.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+/**
+ * CORS setup
+ *
+ * @param req - request
+ * @param res - response
+ * @param next - next function
+ */
+export default (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control=Allow-Methods', 'GET, PATCH, POST');
+        return res.json({});
+    }
+
+    next();
+}
---
+++ src/middlewares/routeError.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import HttpError, {HttpStatusCode} from '../utils/httpError';
+
+/**
+ * Route error
+ *
+ * @param req - request
+ * @param res - response
+ * @param next - next function
+ */
+export default (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const error: HttpError = new HttpError(HttpStatusCode.NotFound, 'Not Found');
+
+    res.status(error.status).json({
+        error: error.message,
+    }).send();
+}
