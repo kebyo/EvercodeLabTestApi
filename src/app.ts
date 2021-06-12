ca4667171fdb7aae1299609d7ed197656f4f242f
Oybek <oybekmuslimov15@gmail.com>
2021-06-12 20:12:19 +0300

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import Env from './env';
+import cors from './middlewares/cors';
+import routeError from './middlewares/routeError';
+
+/**
+ * EvercodeLab Test task API
+ */
+export default class EvercodeLabTestApi {
+    /**
+     * Express.js application
+     */
+    private app: express.Application = express();
+
+    /**
+     * Host
+     */
+    private host: string = Env.host;
+
+    /**
+     * Port
+     */
+    private port: number = Env.port;
+
+    /**
+     * Constructor
+     */
+    public constructor() {
+        this.app.use(express.json());
+        this.app.use(cors);
+        this.app.use(routeError);
+    }
+
+    /**
+     * Run API
+     */
+    public run() {
+        this.app.listen(this.port, this.host, () => {
+            console.log(`⚡️[server]: Server is running at http://${this.host}:${this.port}`)
+        });
+    }
+
+};
