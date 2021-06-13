24fd001ac08a8064a442c5d1648da7b36e963b5b
Oybek <oybekmuslimov15@gmail.com>
2021-06-13 16:21:58 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import Env from './env';
 import cors from './middlewares/cors';
 import routeError from './middlewares/routeError';
+import currency from './routes/currency';
 
 /**
  * EvercodeLab Test task API
@@ -28,6 +29,9 @@ export default class EvercodeLabTestApi {
     public constructor() {
         this.app.use(express.json());
         this.app.use(cors);
+
+        this.app.use('/currency', currency);
+
         this.app.use(routeError);
     }
 
