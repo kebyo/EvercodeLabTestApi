b869cfe63a0e798b392c648787171a22ff842ca1
Oybek <oybekmuslimov15@gmail.com>
2021-06-13 16:21:38 +0300

    Fix comments

--- src/utils/httpError.ts
+++ src/utils/httpError.ts
@@ -30,10 +30,12 @@ export default HttpError;
  */
 export enum HttpStatusCode {
     /**
-     * The server can not find the requested resource. In the browser, this means the URL is not recognized.
-     * In an API, this can also mean that the endpoint is valid but the resource itself does not exist.
-     * Servers may also send this response instead of 403 to hide the existence of a resource from an unauthorized client.
-     * This response code is probably the most famous one due to its frequent occurrence on the web.
+     * The server can not do something. It can be route, entities.
      */
     NotFound = 404,
+
+    /**
+     * The server can not do something because user send bad request. It can be wrong data.
+     */
+    BadRequest = 400,
 }
