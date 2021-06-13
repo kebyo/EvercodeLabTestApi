c2e6a70959d90b68083077f796d1828d1d6a6bbf
Oybek <oybekmuslimov15@gmail.com>
2021-06-13 16:21:21 +0300

    Fix error message

--- src/middlewares/routeError.ts
+++ src/middlewares/routeError.ts
@@ -9,7 +9,7 @@ import HttpError, {HttpStatusCode} from '../utils/httpError';
  * @param next - next function
  */
 export default (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const error: HttpError = new HttpError(HttpStatusCode.NotFound, 'Not Found');
+    const error: HttpError = new HttpError(HttpStatusCode.NotFound, 'Route not Found');
 
     res.status(error.status).json({
         error: error.message,
