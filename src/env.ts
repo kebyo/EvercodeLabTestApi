2b6d0350d92c61e700de0ad73d77b5b18c081a6a
Oybek <oybekmuslimov15@gmail.com>
2021-07-04 23:16:12 +0300

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -4,16 +4,25 @@ import path from 'path';
 dotenv.config({ path: path.join(__dirname, '/../../.env') });
 
 /**
+ * Host for listening by server
+ */
+const host = process.env.HOST as string || '127.0.0.1';
+
+/**
+ * The Port you are using for http connection
+ */
+const port = parseInt(process.env.HTTP_PORT as string || '3000', 10);
+
+/**
+ * API url
+ */
+const apiUrl = `http://${host}:${port}`;
+
+/**
  * Object for settings (data from .env and etc)
  */
 export default {
-    /**
-     * Host for listening by server
-     */
-    host: process.env.HOST as string || '127.0.0.1',
-
-    /**
-     * The Port you are using for http connection
-     */
-    port: parseInt(process.env.HTTP_PORT as string || '3000', 10),
+    host,
+    port,
+    apiUrl,
 }
